065d32cdfb9df3380c96ca9379606ef024c2cc98
Alan Munoz <amunoz@orange.com.do>
2018-12-21 15:45:48 -0400

    front page con responsive beta

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import {
 } from  'react-popupbox';
 import  '../node_modules/react-popupbox/dist/react-popupbox.css'
 import  '../node_modules/bulma/css/bulma.css';
+import NumberFormat from 'react-number-format';
 import  './App.css';
+import  './css/roboto-fonts.css';
+import  './css/custom.css';
+
 class  App  extends  Component {
   constructor(){
       super();
@@ -56,7 +60,13 @@ class  App  extends  Component {
       hab: section.acf.habitaciones,
       ban: section.acf.banos,
       par: section.acf.parqueos,
-      tipo: section.acf.tipo_propiedad
+      tipo: section.acf.tipo_propiedad,
+      precio: section.acf.precio,
+      construccion: section.acf.construccion,
+      sector: section.acf.sector,
+      provincia: section.acf.provincia,
+      desea: section.acf.desea.value,
+      moneda: section.acf.moneda.value
     }
   }
   openPopupbox(section) {
@@ -76,21 +86,84 @@ class  App  extends  Component {
           <Header></Header>
         </header>
         <div>
+        <div id="srp-list" className="srp-list">
+
             {this.scaledSections.map((level, i) =>
-              <div  className="columns"  key={i}>
+              <div className="columns" key={i}>
                 {level.map((section, j) =>
-                  <div  className="column"  key={j}>
-                    <img
-                       className="image"
-                       alt=""
-                       src={section.src}
-                       height={section.img["small-height"]}
-                       onClick={() => this.openPopupbox(this.state.sections[i*3+j])} />
+                  <div className="column" key={j}>
+
+
+
+<ul className="srp-list-marginless list-unstyled prop-list">
+                      <li class="component_property-card js-component_property-card js-quick-view" data-propertyid="272" data-url="http://www.superhogar.net/casa-cuesta-hermosa-ii-2/" data-featured="" data-rank="1" id="272" itemscope="" itemtype="http://schema.org/SingleFamilyResidence" add-lead-form-modal="">
+
+                          <div class="pre-card-wrap">
+                              <div class="broker-info">
+                                  <div class="ellipsis">
+                                      <span>Cortesía de </span>
+                                      <span data-label="property-broker">RE/MAX</span>
+                                  </div>
+                              </div>
+
+                              <div class="label-wrap">
+                                  <span class="label c_label label-green" data-label="property-label-new">
+                                    {section.tipo}
+                                  </span>
+                              </div>
+                          </div>
+
+                          <div class="card-box js-navigate-to js-record-user-activity" id="1156393458" data-remnant="" data-featured-token="" data-url="http://www.superhogar.net/casa-cuesta-hermosa-ii-2/">
+
+                              <div class="photo-wrap " data-label="property-photo">
+                                      <img
+                                        alt="{section.tipo} {section.sector} {section.provincia}"
+                                        src={section.src}
+                                        onClick={() => this.openPopupbox(this.state.sections[i*3+j])} />
+                                        <div class="photo-wrap-detail-overlay"></div>
+                              </div>
+
+                              <div class="detail-wrap ">
+                                  <div class="photo-overlay">
+                                      <div class="property-type">{section.desea}</div>
+                                      <div class="price" data-label="property-price">
+                                          <span class="data-price">
+                                            {section.moneda}<NumberFormat value={section.precio} displayType={'text'} thousandSeparator={true} />
+                                          </span>
+
+                                      </div>
+                                  </div>
+
+                                  <div class="photo-overlay-right">
+                                  </div>
+
+                                  <ul class="prop-meta ellipsis">
+                                      <li data-label="property-meta-beds"><span class="data-value meta-beds">{section.hab}</span> Hab</li>
+                                      <li data-label="property-meta-baths"><span class="data-value">{section.ban}</span> Ban</li>
+                                      <li data-label="property-meta-sqft"><span class="data-value">{section.construccion}</span> mts</li>
+                                      <li data-label="property-meta-lotsize"><span class="data-value"></span><span class="lot-label"> lot</span></li>
+                                      <li data-label="property-meta-garage"><span class="data-value">{section.par}</span> Par</li>
+                                  </ul>
+
+                                  <div class="address ellipsis" data-label="property-address">
+                                      <a href="#">
+                                          <span class="listing-street-address">{section.sector}</span>,
+                                          <span class="listing-city">{section.provincia}</span>,
+                                      </a>
+                                  </div>
+
+
+                              </div>
+
+
+                          </div>
+
+                      </li>      </ul>
                   </div>
                 )}
               </div>
             )}
-      </div>
+      </div>  </div>
       <PopupboxContainer />
     </div>);
   }
